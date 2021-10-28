62070ea36e6dc8281c56912561d43332ee6aff30
Ed Cordero <78165600+eduardocor89@users.noreply.github.com>
2021-10-28 16:24:00 -0400

    Create repos.py

---
+++ repos.py
@@ -0,0 +1,30 @@
+import requests
+import pygal
+from pygal.style import NeonStyle  as NS
+
+# Make an API call and store the response.
+
+URL = 'https://api.github.com/search/repositories?q=language:python&sort=star'
+r = requests.get(URL)
+print("Status code:", r.status_code)
+
+# Store API response in a variable
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+
+names, stars = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization
+my_style = NS(base_style=NS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.tittle = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('',stars)
+chart.render_to_file('python_repos.svg')
